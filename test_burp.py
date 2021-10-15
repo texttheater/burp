d4ee07805e733db6a8341f00afae880efef83c6e
Kilian Evang <noreply@texttheater.net>
2021-10-15 16:05:45 +0200

    Reformatted test cases

--- test_burp.py
+++ test_burp.py
@@ -9,11 +9,36 @@ class BurpTestCase(unittest.TestCase):
 
     def test_burp(self):
         cases = (
-            ('(A 0)', '(A 0)', 0.0),
-            ('(A (B 0))', '(A 0)', 1.0),
-            ('(A (B (C (F 0))))', '(B (D (E (F 0))))', 3.0),
-            ('(A (B (C (F 0) (G 1))))', '(B (D (E (F 0) (G 1))))', 3.0),
-            ('(A (B (C (F 0) (G 1) (H (I 2)))))', '(B (D (E (F 0) (G 1) (I 2))))', 4.0),
+            (
+                '(A 0)',
+                '(A 0)',
+                0.0,
+            ),
+            (
+                '(A (B 0))',
+                '(A 0)',
+                1.0,
+            ),
+            (
+                '(A (B (C (F 0))))',
+                '(B (D (E (F 0))))',
+                3.0,
+            ),
+            (
+                '(A (B (C (F 0) (G 1))))',
+                '(B (D (E (F 0) (G 1))))',
+                3.0,
+            ),
+            (
+                '(A (B (C (F 0) (G 1) (H (I 2)))))',
+                '(B (D (E (F 0) (G 1) (I 2))))',
+                4.0,
+            ),
+            (
+                '(A (B (C (F 0) (G 1) (H (I 2))) (J 3)))',
+                '(B (D (E (F 0) (G 1) (I 2) (J 3))))',
+                5.0,
+            ),
         )
         for tree1, tree2, distance in cases:
             tree1 = ParentedTree(tree1)
